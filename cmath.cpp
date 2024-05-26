a2e59695a2aa7ba11f823dde88f4a576d8d3e2da
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-26 21:45:59 +0530

    Add files via upload

---
+++ cmath.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    double num = 1.3;
+    double Floor = floor(num);
+    double Ceil = ceil(num);
+
+    cout<<"Floored "<<num<<" is: "<<Floor<<endl<<"Ceil "<<num<<" is: "<<Ceil<<endl;
+
+    double square = pow(2,2);
+
+    cout<<"Square of 2 is: "<<square<<endl;
+
+    return 0;
+
+
+}
