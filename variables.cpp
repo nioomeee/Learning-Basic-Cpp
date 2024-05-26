e8d025a5ec9923416a65a94f8f561799184bc4dd
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-26 22:12:00 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double price = 99.99;
+
+    float interestRate = 3.97f; //writing F/f is verrrryy imp while declaring a float 
+    // otherwise compiler will treat it as double
+
+    long fileSize = 90000L; //writing L/l is very imp otherwise treated as an integer
+
+    char letter = 'n';
+
+    bool isValid = true;
+    
+    return 0;
+}
