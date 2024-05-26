356e270e3c352eba2be78442a1b8e7b975de9e69
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-26 22:12:23 +0530

    Add files via upload

---
+++ auto.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    auto price = 99.99;
+
+    auto interestRate = 3.97f;
+
+    auto fileSize = 90000L;
+
+    auto letter = 'n';
+
+    auto isValid = true;
+    
+    return 0;
+}
