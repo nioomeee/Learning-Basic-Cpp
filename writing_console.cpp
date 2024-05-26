36267fe09586b6013865b021723488dba92a5721
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-26 20:46:00 +0530

    Add files via upload

---
+++ writing_console.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10, y =5, a = 1, b = 2;
+    cout<<"x = "<<x << endl;
+    cout<<"y = "<<y << endl;
+
+    cout<<"a = "<<a<<endl
+        <<"b = "<<b;
+    return 0;
+}
